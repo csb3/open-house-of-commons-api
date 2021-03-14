8c16161caf0ad3d88fc7dd53bbe8145f23e5f907
Melanie Tong <melaniietong@gmail.com>
2021-03-14 00:43:00 +0000

    Fix to work with a single MP id

--- db/parsing/mps_parsing.js
+++ db/parsing/mps_parsing.js
@@ -6,60 +6,51 @@ const parser = new xml2js.Parser();
 const concat = require('concat-stream');
 const db = require('../index.js');
 
-const sqlQuery = 'INSERT INTO mps (id, first_name, last_name, party_id, constituency_id) VALUES ($1, $2, $3, $4, $5)';
+const sqlQuery = 'INSERT INTO mps (id, first_name, last_name, party_name, constituency_id) VALUES ($1, $2, $3, $4, $5)';
 const mpQuery = 'SELECT mp_id FROM mp_votes GROUP BY mp_id';
-const constQuery = 'SELECT const_id FROM constituencies WHERE name=$1;';
+const constQuery = 'SELECT id FROM constituencies WHERE name=$1;';
 
 parser.on('error', (err) => { console.log('Parser error', err); });
 
-const seedingMPs = function () {
+
+
+
+const seedingMPs = function() {
+  let exampleId;
   db.query(mpQuery)
-    .then((res) => {
-      res.rows.forEach((row) => {
-        const mpId = row.mp_id;
-
-        https.get(`https://www.ourcommons.ca/Members/en/${mpId}/xml`, (resp) => {
-          resp.on('error', (err) => {
-            console.log('Error while reading', err);
-          });
-
-          resp.pipe(concat((buffer) => {
-            const str = buffer.toString();
-
-            parser.parseString(str, (err, result) => {
-              const constParams = [result.MemberOfParliamentRole.ConstituencyName];
-              db.query(constQuery, constParams)
-                .then((res2) => {
-                  console.log(res2.rows);
-                  const mpParams = [
-                    mpId,
-                    result.MemberOfParliamentRole.PersonOfficialFirstName[0],
-                    result.MemberOfParliamentRole.PersonOfficialLastName[0],
-                    result.MemberOfParliamentRole.CaucusShortName[0],
-                    res2.rows[0].id,
-                  ];
-                  db.query(sqlQuery, mpParams);
-                });
-            });
-          }));
-        });
-      });
-    })
-    .catch(err => console.log(err));
+    .then((res) => exampleId = res.rows[0].mp_id)
+    .then(() => console.log(exampleId));
 };
+    
+// seedingMPs();
+    
+    
+https.get('https://www.ourcommons.ca/members/en/88292/xml', function(resp) {
+  resp.on('error', function(err) {
+    console.log('Error while reading', err);
+  });
 
-seedingMPs();
+  resp.pipe(concat(function(buffer) {
+    let str = buffer.toString();
 
-// https.get('https://www.ourcommons.ca/members/en/search/xml', function(resp) {
-//   resp.on('error', function(err) {
-//     console.log('Error while reading', err);
-//   });
+    parser.parseString(str, function(err, result) {
+      const findByMP = result.Profile.MemberOfParliamentRole[0]
+      const constParams = [findByMP.ConstituencyName[0]];
 
-//   resp.pipe(concat(function(buffer) {
-//     let str = buffer.toString();
+      db.query(constQuery, constParams)
+        .then((res) => {
 
-//     parser.parseString(str, function(err, result) {
-//       seedingMPs(result);
-//     });
-//   }));
-// });
+          const mpParams = [
+            88292,
+            findByMP.PersonOfficialFirstName[0],
+            findByMP.PersonOfficialLastName[0],
+            findByMP.CaucusShortName[0],
+            res.rows[0].id
+          ];
+
+          db.query(sqlQuery, mpParams)
+        })
+        .catch(err => console.log(err));
+    });
+  }));
+});
