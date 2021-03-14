e2e7c471c622bcf3165ed62a086bf138dd3fb7bf
Melanie Tong <melaniietong@gmail.com>
2021-03-14 00:54:55 +0000

    Fix async function

--- db/parsing/mps_parsing.js
+++ db/parsing/mps_parsing.js
@@ -12,45 +12,49 @@ const constQuery = 'SELECT id FROM constituencies WHERE name=$1;';
 
 parser.on('error', (err) => { console.log('Parser error', err); });
 
+const asyncLoop = async function(mpIdArray) {
+  for (const mpId of mpIdArray) {
+    try {
 
-
+      https.get(`https://www.ourcommons.ca/members/en/${mpId.mp_id}/xml`, function(resp) {
+        resp.on('error', function(err) {
+          console.log('Error while reading', err);
+        });
+      
+        resp.pipe(concat(function(buffer) {
+          let str = buffer.toString();
+      
+          parser.parseString(str, function(err, result) {
+            const findByMP = result.Profile.MemberOfParliamentRole[0]
+            const constParams = [findByMP.ConstituencyName[0]];
+      
+            db.query(constQuery, constParams)
+              .then((res) => {
+      
+                const mpParams = [
+                  mpId.mp_id,
+                  findByMP.PersonOfficialFirstName[0],
+                  findByMP.PersonOfficialLastName[0],
+                  findByMP.CaucusShortName[0],
+                  res.rows[0].id
+                ];
+      
+                db.query(sqlQuery, mpParams)
+              })
+              .catch(err => console.log(err));
+          });
+        }));
+      });
+    } catch(error) {
+      console.log(error, "NAY");
+    }
+  }
+};
 
 const seedingMPs = function() {
-  let exampleId;
   db.query(mpQuery)
-    .then((res) => exampleId = res.rows[0].mp_id)
-    .then(() => console.log(exampleId));
+    .then((res) => asyncLoop(res.rows))
+    .catch(err => console.log(err));
 };
-    
-// seedingMPs();
-    
-    
-https.get('https://www.ourcommons.ca/members/en/88292/xml', function(resp) {
-  resp.on('error', function(err) {
-    console.log('Error while reading', err);
-  });
 
-  resp.pipe(concat(function(buffer) {
-    let str = buffer.toString();
-
-    parser.parseString(str, function(err, result) {
-      const findByMP = result.Profile.MemberOfParliamentRole[0]
-      const constParams = [findByMP.ConstituencyName[0]];
-
-      db.query(constQuery, constParams)
-        .then((res) => {
-
-          const mpParams = [
-            88292,
-            findByMP.PersonOfficialFirstName[0],
-            findByMP.PersonOfficialLastName[0],
-            findByMP.CaucusShortName[0],
-            res.rows[0].id
-          ];
-
-          db.query(sqlQuery, mpParams)
-        })
-        .catch(err => console.log(err));
-    });
-  }));
-});
+seedingMPs();
