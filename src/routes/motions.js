8183dbbcc3db1c17e8206c309908da7889a6bf02
Melanie Tong <melaniietong@gmail.com>
2021-03-16 00:48:13 +0000

    Add initial motions route

---
+++ src/routes/motions.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router  = express.Router();
+
+const db = require('./../../db/index');
+
+router.get('/', (req, res) => {
+  db.query(`SELECT * FROM motions;`, []).then(
+    response => res.send(response.rows)
+  );
+});
+
+// router.post('/', (req, res) => {
+//   // Code goes here.
+// });
+
+module.exports = router;
