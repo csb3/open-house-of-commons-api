82049c22ae78a7f864f408e7b6c5d21cd9f766ac
Melanie Tong <melaniietong@gmail.com>
2021-03-16 00:48:37 +0000

    Temp app.use motions

--- src/index.js
+++ src/index.js
@@ -8,12 +8,12 @@ const app = express();
 // Just to check if db is working correctly.
 // Get rid of these lines once there are actual quries.
 const db = require('./../db/index');
-db.query(`SELECT * FROM mp_votes;`, []).then(res => console.log(res.rows));
+db.query(`SELECT * FROM motions;`, []).then(res => console.log(res.rows));
 
 // Example code for importing route file
-const route_example = require('./routes/route_example');
+const motions = require('./routes/motions');
 
 // localhost:3001/route_example will display 'Hello world'
-app.use('/route_example', route_example);
+app.use('/motions', motions);
 
 app.listen(3001, () => console.log('Now browse to localhost:3001'));
