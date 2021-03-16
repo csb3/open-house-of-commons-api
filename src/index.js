b362dff7afd02720dc98d96333a7b13ceebb3aad
Melanie Tong <melaniietong@gmail.com>
2021-03-16 03:25:42 +0000

    Add app use mp_votes

--- src/index.js
+++ src/index.js
@@ -5,15 +5,10 @@ const express = require('express');
 
 const app = express();
 
-// Just to check if db is working correctly.
-// Get rid of these lines once there are actual quries.
-const db = require('./../db/index');
-db.query(`SELECT * FROM motions;`, []).then(res => console.log(res.rows));
-
-// Example code for importing route file
 const motions = require('./routes/motions');
+const mp_votes = require('./routes/mp_votes');
 
-// localhost:3001/route_example will display 'Hello world'
 app.use('/motions', motions);
+app.use('/mp_votes', mp_votes);
 
 app.listen(3001, () => console.log('Now browse to localhost:3001'));
