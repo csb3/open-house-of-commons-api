507ef1a64c9cd24d198a2f3d7afe835a1639e66f
Melanie Tong <melaniietong@gmail.com>
2021-03-16 03:37:29 +0000

    Change listen console message

--- src/index.js
+++ src/index.js
@@ -11,4 +11,4 @@ const mp_votes = require('./routes/mp_votes');
 app.use('/motions', motions);
 app.use('/mp_votes', mp_votes);
 
-app.listen(3001, () => console.log('Now browse to localhost:3001'));
+app.listen(3001, () => console.log('Now listening on localhost:3001...'));
