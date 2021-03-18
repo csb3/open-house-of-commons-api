18883eab1edc4b4056c1810194783c8719c6cc2b
Cass Bredeson <c.s.bredeson@gmail.com>
2021-03-18 17:38:56 -0600

    add login route

---
+++ src/routes/login.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router  = express.Router();
+
+const { db } = require('./../../db/index');
+
+router.get('/:id', (req, res) => {
+  db.query(`SELECT email FROM users WHERE id = $1;`, [req.params.id])
+    .then(
+      response => res.send(response.rows)
+    );
+});
+
+module.exports = router;
