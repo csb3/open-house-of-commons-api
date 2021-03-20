596ccabb3018115c5ee6f43e09657b35f8519c17
Cass Bredeson <c.s.bredeson@gmail.com>
2021-03-20 20:54:48 +0000

    add constituencies to mp votes query

--- src/routes/motions.js
+++ src/routes/motions.js
@@ -1,45 +1,48 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 
-const { db } = require('./../../db/index');
+const { db } = require("./../../db/index");
 
-router.get('/', (req, res) => {
-  db.query(`SELECT * FROM motions;`, []).then(
-    response => res.send(response.rows)
+router.get("/", (req, res) => {
+  db.query(`SELECT * FROM motions;`, []).then((response) =>
+    res.send(response.rows)
   );
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   const responseObj = {};
   db.query(`SELECT * FROM motions WHERE id = $1`, [req.params.id])
-    .then(result=> {
+    .then((result) => {
       if (result.rows.length !== 0) {
         responseObj.motionInfo = result.rows;
       } else {
-        res.send('error');
+        res.send("error");
       }
     })
     .then(() => {
-      return db.query(`SELECT * FROM mps FULL JOIN (SELECT voted_yea, voted_nay, vote_paired, mp_id from mp_votes WHERE motion_id = $1) as Foo  on mps.id = foo.mp_id ORDER BY mps.party_name, mps.last_name;`, [req.params.id])
-        .then(response => {
+      return db
+        .query(
+          `SELECT *, constituencies.* FROM mps FULL JOIN (SELECT voted_yea, voted_nay, vote_paired, mp_id from mp_votes WHERE motion_id = $1) as Foo  on mps.id = foo.mp_id LEFT JOIN constituencies on mps.constituency_id = constituencies.id ORDER BY mps.party_name, mps.last_name;`,
+          [req.params.id]
+        )
+        .then((response) => {
           responseObj.voteInfo = response.rows;
         });
     })
     .then(() => {
-      db.query(`SELECT
+      db.query(
+        `SELECT
         (SELECT count(*) from user_votes WHERE voted_yea = true AND motion_id = $1) as YesVotes,
-        (SELECT count(*) from user_votes WHERE voted_nay = true AND motion_id = $2) as NoVotes`, [req.params.id, req.params.id])
-        .then(response => {
-          responseObj.userVotes = response.rows;
-          res.send(responseObj);
-        });
+        (SELECT count(*) from user_votes WHERE voted_nay = true AND motion_id = $2) as NoVotes`,
+        [req.params.id, req.params.id]
+      ).then((response) => {
+        responseObj.userVotes = response.rows;
+        res.send(responseObj);
+      });
     })
-    .catch(err => {
-      res
-        .status(500)
-        .json({ error: err.message });
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
     });
-
 });
 
 // router.post('/', (req, res) => {
