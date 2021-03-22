372af0bb977e5b3dfecd444b67ed35caeb7b75aa
Cass Bredeson <c.s.bredeson@gmail.com>
2021-03-21 21:46:22 -0600

    add console log to signup form result

--- src/routes/signup.js
+++ src/routes/signup.js
@@ -6,8 +6,9 @@ const { db } = require('./../../db/index');
 router.post('/', (req, res) => {
   console.log("Req.body: ", req.body);
   db.query(`INSERT INTO users (email, password_digest) VALUES ($1, $2) RETURNING email`, [req.body.email, req.body.password])
-    .then(() => {
-      res.send();
+    .then((result) => {
+      console.log("result in signup api", result);
+      res.send(result.rows);
     });
 });
 
